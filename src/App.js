5df94c82a6f4752048d5ea5d76541a0303cdf2ff
Nikoleta Sino <nikoletasino4@gmail.com>
2023-08-14 12:18:51 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+          <h2>Welcome to my First React App.</h2>
+              <h3>This is a very simple example of a basic app made by me! </h3>
+    </div>
+  );
+}
+
+export default App;
